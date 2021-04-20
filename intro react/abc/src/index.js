673e3232b42c19a0d92d5c4a84a6b4449427d0f4
webforshafayet <shohan123boss@gmail.com>
2021-04-20 10:55:46 +0600

    simple array useing map commit

--- intro react/abc/src/index.js
+++ intro react/abc/src/index.js
@@ -6,22 +6,40 @@ const name= "shafayet";
 const id= "12";
 const dept= "CS";
 
+
+const Users=[
+
+{
+		id: '1',
+		name: 'Alamin',
+		dept: 'CS',
+		email: 'alamin@aiub.edu'
+	},
+	{
+		id: '2',
+		name: 'ABC',
+		dept: 'CIS',
+		email: 'abc@aiub.edu'
+	},
+	{
+		id: '3',
+		name: 'XYZ',
+		dept: 'CSE',
+		email: 'xyz@aiub.edu'
+	}
+];
+
+const names =[ 'a' , 'b', 'c' , 'd']
+
+
+
 function Userlist(){
     return(
-        <div>
-            <User name={name} id={id} dept={dept}/>
-            <User name={name} id={id} />
-            <User name= "xyz"/>
-
-
-            {/* <User/>
-            <User/>
-            <User/> */}
-        </div> 
-
-        
-    )
-};
+        names.map((name)=>{
+            return <h1> {name}</h1>
+        })    
+    );
+}
 
 
 // function User(){
@@ -31,4 +49,40 @@ function Userlist(){
 //     // const dept= "CS";
 
 //     return(
-//         <di
+//         <div className='usermain'>
+//          <h1> Name: {name}</h1>
+//          <p>  ID:  {id}</p>
+//          <p>  Dept:  {dept} </p>
+//         </div>  
+//     )
+// };
+
+
+
+//function User(props){
+function User({name, id, dept}){
+
+   // const {name, id, dept} =props;
+
+    // const name= "shafayet";
+    // const id= "12";
+    // const dept= "CS";
+
+    return(
+        <div className='usermain'>
+        {/* <h1> Name: {props.name}</h1>
+        <p>  ID:  {props.id}</p>
+        <p>  Dept:  {props.dept} </p> */}
+
+         <h1> Name: {name}</h1>
+         <p>  ID:  {id}</p>
+         <p>  Dept:  {dept} </p>
+        </div>  
+    )
+};
+
+// ReactDOM.render( <User/>,document.getElementById('root')) /// render  only user
+
+ 
+
+ReactDOM.render( <Userlist/>,document.getElementById('root')) // rendeer userlist which has more user
